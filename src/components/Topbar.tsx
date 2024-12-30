013ffd678792d5409432b379ffd5289ec0917f96
Chenjira (Jane) <chenjira.k@comseven.com>
2024-12-31 00:34:41 +0700

    redirect to topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import MainButton from "./Button/MainButton";
+import MainButton from './Button/MainButton';
 type TopbarProps = {
-   onClickShowMenu?: () => void;
+  onClickShowMenu?: () => void;
 };
 
-const Topbar : React.FC<TopbarProps> = ({ onClickShowMenu }) => {
-   return (
-      <div className="fixed z-10 h-72px sm:h-60px bg-primary flex items-center justify-between w-full px-6">
-         <div className="text-white">A Board</div>
-         <button className="block sm:hidden cursor-pointer" onClick={onClickShowMenu}>
-            <Icon className="text-white" icon={faBars} />
-         </button>
-         <div className="hidden sm:block"><MainButton onClick={() => {}}>Sign In</MainButton></div>
+const Topbar: React.FC<TopbarProps> = ({ onClickShowMenu }) => {
+  const navigator = useNavigate();
+
+  function goToLogin() {
+    navigator('/login');
+  }
+  return (
+    <div className="fixed z-10 h-72px sm:h-60px bg-primary flex items-center justify-between w-full px-6">
+      <div className="text-white">A Board</div>
+      <button
+        className="block sm:hidden cursor-pointer"
+        onClick={onClickShowMenu}
+      >
+        <Icon className="text-white" icon={faBars} />
+      </button>
+      <div className="hidden sm:block">
+        <MainButton onClick={goToLogin}>Sign In</MainButton>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Topbar;
